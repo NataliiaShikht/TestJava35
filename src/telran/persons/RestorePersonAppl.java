8c33e667972151caca016954686779e691c46446
Gregory <Gregory@DESKTOP-U0GR370>
2020-05-04 04:50:26 -0700

    First commit

---
+++ src/telran/persons/RestorePersonAppl.java
@@ -0,0 +1,12 @@
+package telran.persons;
+
+import java.io.IOException;
+
+public class RestorePersonAppl {
+
+	public static void main(String[] args) throws ClassNotFoundException, IOException {
+	
+
+	}
+
+}
